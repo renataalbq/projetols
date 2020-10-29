bb7a3e2a0b41a4bcaa7a623270f860851ef3b9f7
renataalbq <renataalbq1@gmail.com>
2020-10-29 14:50:22 -0300

    adicionando hora dinamicamente

--- js/main.js
+++ js/main.js
@@ -2,32 +2,58 @@ var cadastrar = document.querySelector("#cadastrar");
 
 cadastrar.addEventListener("click", function(event) {
    event.preventDefault();
-
+ 
    var form = document.querySelector("#formulario");
 
    var modelo = form.modelo.value;
    var placa = form.placa.value;
    var horaEntrada = new Date();
+   var horaFormat = `${horaEntrada.getHours()}:${horaEntrada.getMinutes()}`
+
+   if (!modelo){
+      alert("Preencha todos os campos!");
+		return false;
+   }
+
+   else if (!placa){
+      alert("Preencha todos os campos!");
+		return false;
+   }
+
+   else if (!modelo && !placa){
+      alert("Preencha todos os campos!");
+		return false;
+   }
+
 
    var veiculoTr = document.createElement("tr");
 
    var modeloTd = document.createElement("td");
    var placaTd = document.createElement("td");
    var horaTd = document.createElement("td")
-   var finalizarTd = document.createElement("td")
+   var finalizarTd = document.createElement("button")
    var excluirTd = document.createElement("td")
+
+   //finalizarTd.className = 'btnTime';
+   //var img = document.createElement('div');
+   //img.className = 'image';
+   //img.style.backgroundImage = 'url(images/time.png)';
+   //finalizarTd.appendChild(img);
 
    modeloTd.textContent = modelo;
    placaTd.textContent = placa;
+   horaTd.textContent = horaFormat;
 
    veiculoTr.appendChild(modeloTd);
    veiculoTr.appendChild(placaTd);
    veiculoTr.appendChild(horaTd)
    veiculoTr.appendChild(finalizarTd);
    veiculoTr.appendChild(excluirTd)
-
-
+   
    var tabela = document.querySelector("#garagem");
 
    tabela.appendChild(veiculoTr);
+
 });
+
+
