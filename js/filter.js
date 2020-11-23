8a0e563bbdb9b9d8e0a74d7d2be93736beae47bf
renataalbq <renataalbq1@gmail.com>
2020-11-23 19:10:28 -0300

    removi sessionStorage

--- js/filter.js
+++ js/filter.js
@@ -1,14 +1,26 @@
 var search = document.querySelector("#search-input");
 var tabela = document.querySelector("#garagem");
-var form = document.querySelector("#formulario")
-var placa = form.placa.value
 
-search.onkeyup = function(){
-    var filtro = search.value;
-    for (var i = 0; i < tabela.rows.length; i++){
-        var placa = tabela.rows[i].cells[1].innerText;
-        console.log(placa)
-        var corresponde = placa.toLowerCase().indexOf(filtro) >= 0;
-        tabela.rows[i].style.display = corresponde ? '' : 'none';
+search.addEventListener("input", function() {
+    var veiculosTr = document.querySelectorAll(".veiculoTr");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < veiculosTr.length; i++) {
+            var veiculo = veiculosTr[i];
+            var tdPlaca = veiculo.querySelector(".placaBody");
+            var placa = tdPlaca.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (!expressao.test(placa)) {
+                veiculo.classList.add("invisivel");
+            } else {
+                veiculo.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < veiculosTr.length; i++) {
+            var veiculo = veiculosTr[i];
+            veiculo.classList.remove("invisivel");
+        }
     }
-}   
+});
--- js/remove.js
+++ js/remove.js
@@ -1,16 +1,6 @@
-function remove(placa){
+function remove(){
     var td = document.querySelector("td")
     td.parentNode.remove()
-
-    //excluindo do sessionStorage
-    var garagem = JSON.parse(sessionStorage.getItem('veiculo'));
-    console.log(garagem);
-	for(var i = 0 ; i < garagem.length; i++){
-		if(garagem[i].placa == placa){
-			garagem.splice(i, 1);
-		}
-	}
-    sessionStorage.setItem('veiculo', JSON.stringify(garagem));
 };
 
 function check(placa,hora,minuto){
@@ -18,7 +8,7 @@ function check(placa,hora,minuto){
     result = tempoPercorrido
     var c = confirm(`${result}\nDeseja realmente excluir? `);
     if (c) {
-        remove(placa);
+        remove();
     }
     else{
         return false;
